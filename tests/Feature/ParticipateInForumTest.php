1c0068cc55c82aabbf34f5c485dfa9e41b23a736
Nicole <nicole929chan@gmail.com>
2017-09-04 14:17:33 +0000

    validation

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -21,7 +21,7 @@ class ParticipateInForumTest extends TestCase
     /** @test */
     public function an_authenticated_user_may_participate_in_forum_threads()
     {
-        $this->singIn();
+        $this->signIn();
 
     	$thread = create('App\Thread');
 
@@ -32,5 +32,18 @@ class ParticipateInForumTest extends TestCase
     	$this->get($thread->path())
     	    ->assertSee($reply->body);
     }
+
+    /** @test */
+    public function a_reply_requires_a_body() 
+    {
+        $this->withExceptionHandling()->signIn();
+
+        $thread = create('App\Thread');
+
+        $reply = make('App\Reply', ['body' => null]);
+
+        $this->post($thread->path().'/replies', $reply->toArray())
+            ->assertSessionHasErrors('body');
+    } 
         
 }
