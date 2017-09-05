b2d35e41754401450df9b6fea25e183a1ee4c7e6
Nicole <nicole929chan@gmail.com>
2017-09-05 05:22:15 +0000

    validation errors

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,6 +40,8 @@
                     <ul class="nav navbar-nav">
                         <li><a href="/threads">All Threads</a></li>
 
+                        <li><a href="/threads/create">New Thread</a></li>
+
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expended="false">Channels <span class="caret"></span></a>
 
--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -13,8 +13,18 @@
                         {{ csrf_field() }}
 
                         <div class="form-group">
+                            <label for="channel_id">Choose a channel:</label>    
+                            <select class="form-control" name="channel_id" id="channel_id">
+                                <option>Choose one...</option>
+                                @foreach(App\Channel::all() as $channel)
+                                    <option value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected' : '' }}>{{ $channel->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group">
                             <label for="title">Title: </label>
-                            <input type="text" name="title" class="form-control">
+                            <input type="text" name="title" class="form-control" value="{{ old('title') }}">
                         </div>                        
 
                         <div class="form-group">
@@ -22,7 +32,17 @@
                             <textarea name="body" class="form-control" rows="8"></textarea>
                         </div>
 
-                        <button type="submit" class="btn btn-primary">Publish</button>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary">Publish</button>
+                        </div>
+
+                        @if(count($errors))
+                            <ul class="alert alert-danger">
+                                @foreach($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        @endif
                         
                     </form>
 
