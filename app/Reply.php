5c1a9d552c8c4169a34780c41f9b8b53c6ef0a3c
Nicole <nicole929chan@gmail.com>
2017-09-11 14:45:22 +0000

    global scopes

--- app/Reply.php
+++ app/Reply.php
@@ -6,29 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
+	use Favoritable;
+
 	protected $guarded = [];
+
+	protected $with = ['owner', 'favorites'];
 	
     public function owner()
     {
     	return $this->belongsTo(User::class, 'user_id');
     }
 
-    public function favorites()
-    {
-    	return $this->morphMany(Favorite::class, 'favorited');
-    }
-
-    public function favorite()
-    {
-    	if (! $this->favorites()->where(['user_id' => auth()->id()])->exists() ) {
-    		$this->favorites()->create(['user_id' => auth()->id() ]);
-    	}
-    }
-
-    public function isFavorited()
-    {
-    	return $this->favorites()->where(['user_id' => auth()->id()])->exists();
-    }
+     
         
         
         
--- app/Thread.php
+++ app/Thread.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
-	protected $guarded = [];
+    protected $guarded = [];
+	
+    protected $with = ['creator', 'channel'];
 
     protected static function boot()
     {
@@ -15,6 +17,7 @@ class Thread extends Model
         static::addGlobalScope('replyCount', function($builder) {
             $builder->withCount('replies');
         });
+
     }
 
     public function path()
@@ -24,9 +27,7 @@ class Thread extends Model
 
     public function replies()
     {
-    	return $this->hasMany(Reply::class)
-            ->withCount('favorites')
-            ->with('owner');
+    	return $this->hasMany(Reply::class);
     }
 
     public function creator()
